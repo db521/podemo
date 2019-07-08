e800f88f277a6bf702866231c65a421969da5f58
zdl <745887513@qq.com>
2019-07-08 21:07:53 +0800

    调整测试脚本代码

--- testscripts/login.py
+++ testscripts/login.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*- 
 # @Time : 2019/6/30 16:47 
-# @File : loginz.py
+# @File : login.py
 import json
 
 from module.login import loginStep
@@ -10,10 +10,7 @@ from selenium import webdriver
 
 def testloginZentao():
     """
-    登陆页面，获取用户名和密码，是通过excel文件解析
-    页面的所有元素定义在pageobject文件夹下面的loginPage里面
-    所有的定位方式在config里面的pageobject里面
-    这个页面就是综合调用一下
+    具体的测试流程，调用module模块的具体函数
     :return:
     """
     try:
