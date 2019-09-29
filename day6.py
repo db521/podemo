0a34fe86a9644fe408de2ad1716238e8c3ddb58a
xiaomuzikun <1097799424@qq.com>
2019-09-29 10:35:13 +0800

    zongjie

---
+++ day6.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# from selenium import webdriver
+# import time
+# ch = webdriver.Chrome()
+# time.sleep(5)
+#
+#
+# a = 1
+# def fun():
+#     a = 2
+#     # return a
+# print(fun())
+# print(a)
+
+
+
+
+
+
+
