effe6b5d870a91cfd120263123591a66f8cf16fd
zjx <847934243@qq.com>
2019-11-18 10:01:38 +0800

    666

--- util/excelparse.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*- 
-# @Time : 2019/6/30 16:56 
-# @File : excelparse.py 
-import openpyxl
-class ParseExcel:
-    def readExcel(self,excelpath):
-        self.workbook=openpyxl.load_workbook(excelpath)
